d38d60852f8084dc1168d447fec158137b892a58
TUANPC\tranq <nolan.tran@alpaca.vn>
2022-12-12 19:17:25 +0700

    get cookie

--- view/index.php
+++ view/index.php
@@ -386,6 +386,12 @@
 <span id="themesBtn"></span>
 
 <script>
+	window.onload = function() {
+    if(!window.location.hash) {
+        window.location = window.location + '#loaded';
+        window.location.reload();
+    }
+}
 </script>
 </body>
 </html>
