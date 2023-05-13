3041e1d6c9fdeec61754118ea461c168f8d53c04
Quốc Tuấn <76949884+tqt-coder@users.noreply.github.com>
2023-05-13 10:35:27 +0700

    update table

--- model/sql.sql
+++ model/sql.sql
@@ -1,7 +1,7 @@
 
 
 CREATE TABLE `phonecase` (
-  `id` int(11) NOT NULL PRIMARY KEY,
+  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(250) NOT NULL,
   `price` double NOT NULL,
   `value` int(11) NOT NULL,
@@ -11,4 +11,4 @@ CREATE TABLE `phonecase` (
 
 INSERT INTO `phonecase` (`name`, `price`, `value`, `description`,`image`) VALUES
 ('Fujifilm FinePix S2950 Digital Camera', 1000, 10, '14 Megapixels. 18.0 x Optical Zoom. 3.0-inch LCD Screen. Full HD photos and 1280 x 720p HD movie capture. ISO sensitivity ISO6400 at reduced resolution. Tracking Auto Focus. Motion Panorama Mode. Face Detection technology with Blink detection and Smile and shoot mode. 4 x AA batteries not included. WxDxH 110.2 ×81.4x73.4mm. Weight 0.341kg (excluding battery and memory card). Weight 0.437kg (including battery and memory card).','phonecase1.jpg'),
-('Fujifilm FinePix S2950 Digital Camera', 1000, 20, '14 Megapixels. 18.0 x Optical Zoom. 3.0-inch LCD Screen. Full HD photos and 1280 x 720p HD movie capture. ISO sensitivity ISO6400 at reduced resolution. Tracking Auto Focus. Motion Panorama Mode. Face Detection technology with Blink detection and Smile and shoot mode. 4 x AA batteries not included. WxDxH 110.2 ×81.4x73.4mm. Weight 0.341kg (excluding battery and memory card). Weight 0.437kg (including battery and memory card).','phonecase1.jpg');
+('Fujifilm FinePix S2950 Digital Camera_2', 2000, 60, '14 Megapixels. 18.0 x Optical Zoom. 3.0-inch LCD Screen. Full HD photos and 1280 x 720p HD movie capture. ISO sensitivity ISO6400 at reduced resolution. Tracking Auto Focus. Motion Panorama Mode. Face Detection technology with Blink detection and Smile and shoot mode. 4 x AA batteries not included. WxDxH 110.2 ×81.4x73.4mm. Weight 0.341kg (excluding battery and memory card). Weight 0.437kg (including battery and memory card) and betiful.','phonecase1.jpg');
