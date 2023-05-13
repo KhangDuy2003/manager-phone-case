fb4ffc90d524ce95db98b4cbe5b3243b992f6507
Quốc Tuấn <76949884+tqt-coder@users.noreply.github.com>
2023-05-13 10:30:48 +0700

    Update sql.sql

--- model/sql.sql
+++ model/sql.sql
@@ -1,19 +1,13 @@
 
 
 CREATE TABLE `phonecase` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL PRIMARY KEY,
   `name` varchar(250) NOT NULL,
   `price` double NOT NULL,
   `value` int(11) NOT NULL,
   `description` text NOT NULL,
   `image` varchar(255)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-ALTER TABLE `phonecase`
-ADD PRIMARY KEY (`id`);
---
--- Dumping data for table `phonecase`
---
-
+);
 
 INSERT INTO `phonecase` (`name`, `price`, `value`, `description`,`image`) VALUES
 ('Fujifilm FinePix S2950 Digital Camera', 1000, 10, '14 Megapixels. 18.0 x Optical Zoom. 3.0-inch LCD Screen. Full HD photos and 1280 x 720p HD movie capture. ISO sensitivity ISO6400 at reduced resolution. Tracking Auto Focus. Motion Panorama Mode. Face Detection technology with Blink detection and Smile and shoot mode. 4 x AA batteries not included. WxDxH 110.2 ×81.4x73.4mm. Weight 0.341kg (excluding battery and memory card). Weight 0.437kg (including battery and memory card).','phonecase1.jpg'),
